8cdb296a85ff6a7b2a23470db123533771ce8891
ruanye <ye.ruan@qianmo-inc.com>
2016-10-14 10:59:01 +0800

    “搭建项目环境“

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<router-view></router-view>
+	</div>
+</template>
+
+<style>
+	html {
+		height: 100%;
+	}
+
+	body {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 100%;
+	}
+</style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+
+import App from './App';
+import Index from './pages/Index';
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+const router = new VueRouter({ history: true });
+/* eslint-disable eol-last */
+router.map({
+  '/': {
+    name: 'index',
+    component: Index,
+  },
+});
+router.start(App, '#app');
