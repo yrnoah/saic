150c7b53c720b6c40ef1669e9d3af27498ba7856
ruanye <ye.ruan@qianmo-inc.com>
2016-10-16 18:00:51 +0800

    添加本地获取头像图片

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,13 +2,44 @@
 	<div class="swiper-slide login">
 		<div class="weui-uploader">
 			<p>添加照片</p>
-			<input id="uploaderInput" class="uploader" type="file" accept="image/*" multiple/>
+      <img :src="file" class="uploadedImg">
+			<input id="uploaderInput" class="uploader" type="file" accept="image/*" multiple v-on:change="chooseFileChange($event)" capture/>
 		</div>
-		<input type="text" placeholder="输入昵称" class="nameInput">
-    <button class="submit">确认</button>
-    <button class="skipBtn" v-link="{name: 'years'}">跳过次步骤&nbsp;&gt;</button>
+		<input type="text" placeholder="输入昵称" class="nameInput" v-model="nameInput">
+		<button class="submit" @click="submit()">确认</button>
+		<button class="skipBtn" v-link="{name: 'years'}">跳过次步骤&nbsp;&gt;</button>
 	</div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        $file: null,
+        file: null,
+        nameInput: null,
+      };
+    },
+    created() {
+      this.$file = document.querySelector('input[type="file"]');
+      console.log(this.$file);
+    },
+    methods: {
+      chooseFileChange(e) {
+        const url = window.URL || window.webkitURL || window.mozURL;
+        const imgfile = e.target.files[0];
+        this.file = imgfile && url.createObjectURL(imgfile);
+      },
+      clearFile() {
+        this.$file.value = '';
+        this.file = null;
+      },
+      submit() {
+        console.log(this.nameInput, this.file);
+      },
+    },
+  };
+</script>
 
 <style scoped>
 	.login {
@@ -42,7 +73,7 @@
 		left: 0;
 		width: 100%;
 		height: 100%;
-		z-index: 1;
+		z-index: 2;
 		opacity: 0;
 		outline: none;
 	}
@@ -57,33 +88,42 @@
 		height: 40px;
 		font-size: 14px;
 		outline: none;
-    display: block;
-    margin: 0 auto;
+		display: block;
+		margin: 0 auto;
 	}
 
-  .submit {
+	.submit {
 		width: 250px;
 		height: 40px;
-    border-radius: 20px;
-    background-color: #7ED321;
-    border: none;
-    color: #FFF;
-    font-size: 14px;
-    font-weight: 100;
-    outline: none;
-    display: block;
-    margin: 40px auto;
-  }
+		border-radius: 20px;
+		background-color: #7ED321;
+		border: none;
+		color: #FFF;
+		font-size: 14px;
+		font-weight: 100;
+		outline: none;
+		display: block;
+		margin: 40px auto;
+	}
 
-  .skipBtn {
-    border: none;
-    color: #FFF;
-    background-color: transparent;
-    padding: 15px 20px;
+	.skipBtn {
+		border: none;
+		color: #FFF;
+		background-color: transparent;
+		padding: 15px 20px;
+		position: absolute;
+		bottom: 15px;
+		right: 5px;
+		outline: none;
+	}
+
+  .uploadedImg {
     position: absolute;
-    bottom: 15px;
-    right: 5px;
-    outline: none;
+		top: 0;
+		left: 0;
+		width: 100px;
+		height: 100px;
+    border-radius: 50px;
+		z-index: 1;
   }
-
 </style>
