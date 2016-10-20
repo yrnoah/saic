e4d4e891a0175a3ba78b5ee26e5ea116188e0cde
flanny <runwei.luo@qianmo-inc.com>
2016-10-20 18:30:42 +0800

    intergrate wechat api

--- build/pro-server.js
+++ build/pro-server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
-var config = require('../config')
-var proxyMiddleware = require('http-proxy-middleware')
-var proxyTable = config.dev.proxyTable
+const config = require('../config')
+const proxyMiddleware = require('http-proxy-middleware')
+const proxyTable = config.dev.proxyTable
+const wechat = require('wechat');
+
+const [token, appid, EncodingAESKey] = ['xjbtoken2333', 'wx144dae9c46a5f240', 'Vggyp5zaDlGoBSW3PDY7QYRDW5MoUPS4lqeFRZlgUpS'];
+
+
+const wechatConfig = {
+  token,
+  appid,
+  EncodingAESKey,
+}
 
 app.use(express.static('dist/'));
+app.use(express.query());
+
+app.use('/wechat', wechat(wechatConfig, (req, res, next) => {
+  console.log('wechatconfig');
+}));
 
 Object.keys(proxyTable).forEach(function (context) {
   var options = proxyTable[context]
