67d6c890ad48b865d66532f515ee5f7e29302ec6
flanny <runwei.luo@qianmo-inc.com>
2016-10-31 14:02:19 +0800

    add wechat

--- build/pro-server.js
+++ build/pro-server.js
@@ -14,13 +14,30 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const swig = require('swig');
 
-
 const [token, appid, EncodingAESKey, appsecret] =
       ['xjbtoken2333', 'wx818254b4c2b5bb7e', '49a35f5b9483e8f0011cf568b69c0d66', '49a35f5b9483e8f0011cf568b69c0d66'];
 const api = new WechatApi(token, appsecret);
 const client = new WechatOauth(appid, appsecret);
 
-const url = client.getAuthorizeURL('/', '123', 'snsapi_userinfo');
+let url;
+app.use(bodyParser.urlencoded({extended: false}));  
+app.use(bodyParser.json());
+
+app.set('views', 'dist/');
+app.set('view engine', 'html');
+app.set('view cache', false);
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'dist'), {'extensions': ['html']}));
+app.engine('html', swig.renderFile);
+app.use(express.query());
+
+app.get('/home', (req, res) => {
+  res.render('index');
+});
 
 const wechatConfig = {
   token,
@@ -46,27 +63,15 @@ function renderError(sendErrorObj) {
   });
 }
 
-app.use(express.static('dist/'));
-// app.use(express.query());
-
-app.use(bodyParser.urlencoded({extended: false}));  
-app.use(bodyParser.json());
 
 
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-app.engine('html', swig.renderFile);
-app.set('view engine', 'html');
-app.set('view cache', false);
 
 
 const serverApi = express.Router();
 let wechat_api;
+
 app.use('/', (req, res, next) => {
-  //使用wechat-api获取JSconfig  
+  //使用wechat-api获取JSconfig
   var param = {
     debug: true,
     jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage'],
@@ -79,15 +84,21 @@ app.use('/', (req, res, next) => {
   api.getJsConfig(param, function (err, result) {
     if(err) {
       console.log(err);
-      next();
     }
-    console.log(result);
     wechat_api = result;
+    next();
   });
+  req.send({ url });
+  next();
+})
+
+app.get('/', (req, res, next) => {
+  url = client.getAuthorizeURL('http://srkfytl.gofriend.me/home', '123', 'snsapi_userinfo');
+  req.redirect(url);
+  next();
 })
 
 serverApi.get('/wechat_api', (req, res) => {
-  console.log('==============api===============');
   res.send({ wechat_api });
 });
 
@@ -96,18 +107,23 @@ serverApi.get('test', (req, res) => {
 })
 
 serverApi.get('/wechat_oauth', (req, res) => {
-  res.send(url);
+  res.redirect(url);
 });
+
+
+// app.get('/')
+
+
 
 app.use('/api', serverApi);
 
 
 
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
 
 
